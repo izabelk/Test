9738a2567f830a956c40c81adc57d28696651d27
izabelk <izi_girl@abv.bg>
2013-08-01 14:46:47 +0300

    little change

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("git");
+            int a;
+            int b = 5;
         }
     }
 }
